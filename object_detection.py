01561c78f5625fb065eeb34a107b9f5441e6f585
Joni McCawley <107073589+jonimccawley@users.noreply.github.com>
2023-10-18 22:34:10 -0500

    Add files via upload

---
+++ object_detection.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+# Load the pre-trained MobileNet SSD model
+net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt", "MobileNetSSD_deploy.caffemodel")
+
+# Define the list of classes the model can detect
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+# Specify the names of the classes you want to detect:
+DESIRED_CLASSES = ["diningtable"]  # Change this list as per your preferences.
+
+# Convert the class names into indices:
+IDX = [CLASSES.index(label) for label in DESIRED_CLASSES if label in CLASSES]
+
+# Start the webcam feed
+cap = cv2.VideoCapture(1)
+
+while True:
+    # Capture each frame from the webcam
+    ret, frame = cap.read()
+
+    # Resize the frame and convert it to a blob
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+
+    # Set the blob as the input to the network
+    net.setInput(blob)
+
+    # Get the detections
+    detections = net.forward()
+
+    # Loop over the detections
+    for i in range(detections.shape[2]):
+        # Extract the confidence of the detection
+        confidence = detections[0, 0, i, 2]
+        
+        # Extract the index of the class label
+        idx = int(detections[0, 0, i, 1])
+        
+        # Filter out weak detections and check if the detected class is in the desired list
+        if confidence > 0.2 and idx in IDX:
+            # Get the bounding box coordinates
+            box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
+            (startX, startY, endX, endY) = box.astype("int")
+            
+            # Draw the bounding box and label on the frame
+            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Display the frame
+    cv2.imshow('Object Detection', frame)
+
+    # Exit the loop when 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object
+cap.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
